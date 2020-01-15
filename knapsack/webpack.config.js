c6a39f1192cd775863b71a99877bf76b174f52d8
Evan Lovely <EvanLovely@users.noreply.github.com>
2020-01-14 16:55:56 -0800

    build: fix CSS refresh stagnancy

--- knapsack/webpack.config.js
+++ knapsack/webpack.config.js
@@ -2,9 +2,13 @@ const { join } = require('path');
 const { createWebPackConfig } = require('@knapsack/build-tools');
 const WebappWebpackPlugin = require('webapp-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackTagsPlugin = require('html-webpack-tags-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
 
-module.exports = createWebPackConfig({
+const isProd = process.env.NODE_ENV === 'production';
+
+const config = createWebPackConfig({
   mainEntries: [join(__dirname, './src/client/')],
   extraSrcDirs: [join(__dirname, './src')],
   dist: join(__dirname, './dist/client'),
@@ -25,10 +29,40 @@ module.exports = createWebPackConfig({
       filename: 'index.html',
     }),
     new ScriptExtHtmlWebpackPlugin({
-      module: [/js$/],
+      module: ['chunk', 'bundle'],
+    }),
+    new CopyPlugin([
+      {
+        from: require.resolve('react/umd/react.development.js'),
+      },
+      {
+        from: require.resolve('react/umd/react.production.min.js'),
+      },
+      {
+        from: require.resolve('react-dom/umd/react-dom.development.js'),
+      },
+      {
+        from: require.resolve('react-dom/umd/react-dom.production.min.js'),
+      },
+    ]),
+    new HtmlWebpackTagsPlugin({
+      tags: [
+        'ks-design-system/ks-design-system.css',
+        isProd ? 'react.production.min.js' : 'react.development.js',
+        isProd ? 'react-dom.production.min.js' : 'react-dom.development.js',
+      ],
+      append: false,
     }),
     new WebappWebpackPlugin(
       require.resolve('@knapsack/design-system/src/assets/favicon.png'),
     ),
   ],
 });
+
+module.exports = {
+  ...config,
+  externals: {
+    ...config.externals,
+    react: 'React',
+  },
+};
